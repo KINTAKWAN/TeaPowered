346e1f38f67e71bfab1a9b6aa2540b14e4f3eb56
christopher veal <info@ebp.tw>
2016-08-18 11:18:15 +0800

    2

--- ConsoleApplication1/ConsoleApplication1/Class1.cs
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -10,5 +10,7 @@ namespace ConsoleApplication1
     {
         public string FirstName { get; set; }
         public string Phone { get; set; }
+
+        public string LastName { get; set; }
     }
 }
